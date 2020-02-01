8131c32ab5235cf602e1ea26d126348a13466346
Frank Lenoci <fcl1118@live.com>
2020-02-01 12:52:18 -0600

    Initial Commit

---
+++ LenociF_HW3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LenociF_HW3
+//
+//  Created by phrank on 2/1/20.
+//  Copyright © 2020 phrank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
