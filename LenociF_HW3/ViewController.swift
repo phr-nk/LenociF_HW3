2285290d877c26f61519e10f56238a9e065591c0
Frank Lenoci <fcl1118@live.com>
2020-02-03 22:51:59 -0600

    initial commit

--- LenociF_HW3/ViewController.swift
+++ LenociF_HW3/ViewController.swift
@@ -8,8 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var num1: Int?
+    var num2: Int?
+    var isReady = false; //determine if the still typing a number
+
+    @IBOutlet weak var display: UILabel!
+    
+    //when the + is pressed
+    @IBAction func addPressed(_ sender: UIButton) {
+        isReady = false
+        num1 = Int(display.text!)
+        
+       
+    }
+    //when equals is pressed
+    @IBAction func equalsPressed(_ sender: UIButton) {
+        isReady = false
+        var res = 0
+        num2 = Int(display.text!) //get second number
+        res = num1! + num2!
+        display.text = String(res)
+        
+    }
+    
+    //get inital number and update display
+    @IBAction func getInitNumber(_ sender: UIButton) {
+        let number = String(sender.currentTitle ?? "0")
+              
+          if isReady
+          {
+              display.text = (display.text ?? "0") + number
+          }
+          else
+          {
+              display.text = number
+              isReady = true
+          }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
